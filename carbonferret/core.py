3c04fdc5555a4b66a249e4764adb0b441e70518c
Brewster Malevich <sbmalev@gmail.com>
2017-06-02 09:43:54 -0700

    Basic unittest for find_near().

--- carbonferret/core.py
+++ carbonferret/core.py
@@ -2,26 +2,29 @@ import requests
 import pandas
 
 
-def find_near(lat, lon, *, n=10):
+def find_near(lat, lon, *, n=10, session=None):
     """Return n results for a given latitude and longitude"""
     search_params = {'npoints': n, 'clat': lat, 'clon': lon, 
                      'Columns[]': ['Subregion', 'Notes', 'CollectionYear', 
                                    'ReservoirAge', 'ReservoirErr', 'C14age', 
                                    'C14err', 'LabID', 'Delta13C', 'nextime', 
                                    'Genus', 'Species', 'Feeding', 'Name']}
-    resp = _query_near(**search_params)
+    resp = _query_near(session=session, **search_params)
     df = _response_to_dataframe(resp)
     df_clean = _clean_dataframe(df)
     return df_clean
 
 
-def _query_near(**kwargs):
+def _query_near(*, session=None, **kwargs):
     """Query marine database with given query string values and keys"""
     url_endpoint = 'http://calib.org/marine/index.html'
-    with requests.Session() as s:
-        # Need to get the index page before query. Otherwise get bad query response that seems legit.
-        s.get('http://calib.org/marine/index.html')
-        resp = s.get(url_endpoint, params=kwargs)
+    if session is not None:
+        resp = session.get(url_endpoint, params=kwargs)
+    else:
+        with requests.Session() as s:
+            # Need to get the index page before query. Otherwise get bad query response that seems legit.
+            s.get('http://calib.org/marine/index.html')
+            resp = s.get(url_endpoint, params=kwargs)
     return resp
 
 
--- carbonferret/tests/test_carbonferret.py
+++ carbonferret/tests/test_carbonferret.py
@@ -1,24 +1,45 @@
 import unittest
 import carbonferret as cf
+import carbonferret.core as cfcore
+from os import path
+
+
+class DummyResponse:
+
+    def __init__(self, payload):
+        self.text = payload
+
+
+class DummySession:
+
+    def __init__(self, testhtml):
+        self.response = DummyResponse(testhtml)
+
+    def get(self, url_endpoint, params):
+        return self.response
 
 
 class TestCarbonferret(unittest.TestCase):
 
-    @unittest.skip('Test not written')
+    def setUp(self):
+        # testresponse.html was taken from calib.org/marine on 2017-05-02
+        here = path.abspath(path.dirname(__file__))
+        testhtml_path = path.join(here, 'testresponse.html')
+        with open(testhtml_path) as fl:
+            testhtml_string = fl.read()
+        self.testhtml_string = testhtml_string
+        self.dummysession = DummySession(testhtml_string)
+
+    # @unittest.skip('Test not written')
     def test_find_near(self):
-        pass
-
-    @unittest.skip('Test not written')
-    def test__query_near(self):
-        pass
-
-    @unittest.skip('Test not written')
-    def test___response_to_dataframe(self):
-        pass
-
-    @unittest.skip('Test not written')
-    def test__clean_dataframe(self):
-        pass
+        victim = cf.find_near(lat=0, lon=0, n=2, session=self.dummysession)
+        # Just testing a couple key values, not everything.
+        goals = {'MapNo': [1221, 1222],
+                'Lon': [-5.7416, -5.7416],
+                'Lat': [-15.9409, -15.9409],
+                'DeltaR': [361, 294]}
+        for k, v in goals.items():
+            self.assertTrue(all(victim[k].values == v))
 
 
 if __name__ == '__main__':
