5569c2717d180813508d44119837b5635384986e
Brewster Malevich <sbmalev@gmail.com>
2017-06-02 08:59:40 -0700

    Added skeleton unittests. They are useless right now.

---
+++ carbonferret/tests/test_carbonferret.py
@@ -0,0 +1,25 @@
+import unittest
+import carbonferret as cf
+
+
+class TestCarbonferret(unittest.TestCase):
+
+    @unittest.skip('Test not written')
+    def test_find_near(self):
+        pass
+
+    @unittest.skip('Test not written')
+    def test__query_near(self):
+        pass
+
+    @unittest.skip('Test not written')
+    def test___response_to_dataframe(self):
+        pass
+
+    @unittest.skip('Test not written')
+    def test__clean_dataframe(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
