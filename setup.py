b64d3a6f9f7862995db12ffecad4c1ee019be7ba
Brewster Malevich <sbmalev@gmail.com>
2017-06-02 10:25:06 -0700

    Added lxml dependency.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'beautifulsoup4', 'html5lib', 'pandas'],
+    install_requires=['requests', 'beautifulsoup4', 'html5lib', 'lxml', 'pandas'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
